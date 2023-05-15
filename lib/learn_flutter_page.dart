3a1ce43c64d7dc19caa63e7f1868de3a1e299394
Daniel Rey <danielrey10252000@gmail.com>
2023-05-15 19:25:26 +0800

    More on stateful widget and other widgets

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -8,6 +8,22 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool switcherEnabled = false;
+  bool? isChecked = false;
+
+  Color helloWorldOneBg = Colors.blueGrey;
+
+  Text createText(String message, Color? color, FontWeight fWeight) => Text(
+        message,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontFamily: 'Roboto',
+          fontSize: 16,
+          fontWeight: fWeight,
+          color: color,
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,13 +40,70 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
       body: Column(
         children: [
           Image.asset("images/galaxy.png"),
-          const SizedBox(
-            height: 10,
-          ),
           const Divider(color: Colors.black),
-          Container(),
+          Container(
+            color: switcherEnabled ? Colors.black : Colors.blueGrey,
+            width: double.infinity,
+            margin: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(15.0),
+            child:
+                createText("Hello World One", Colors.white, FontWeight.normal),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint("Elevated button clicked");
+            },
+            child: createText("Hello World Two", Colors.white, FontWeight.bold),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint("Outlined button clicked");
+            },
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              child: createText(
+                  "Hello World Three", Colors.yellowAccent, FontWeight.bold),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint("Text button clicked");
+            },
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              child:
+                  createText("Hello World Four", Colors.black, FontWeight.bold),
+            ),
+          ),
+          GestureDetector(
+            onTap: () => debugPrint("Gesture detector tapped"),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.linked_camera,
+                  fill: BorderSide.strokeAlignCenter,
+                ),
+                Icon(Icons.post_add),
+                Icon(Icons.fire_extinguisher),
+              ],
+            ),
+          ),
+          Switch(
+              value: switcherEnabled,
+              onChanged: (bool isOn) => setState(() {
+                    switcherEnabled = isOn;
+                  })),
+          Checkbox(
+              value: isChecked,
+              onChanged: (bool? checkState) {
+                setState(() {
+                  isChecked = checkState;
+                });
+              }),
         ],
       ),
     );
+    // 49:35
   }
 }
